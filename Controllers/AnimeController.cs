7f7f1699cc0dd93fa517f155afd8bb0b087e7d89
Higor Henrique Reculiano <higor.reculiano@hotmail.com>
2022-04-10 18:45:28 -0300

    Criação da AnimeController

--- Controllers/AnimeController.cs
+++ Controllers/AnimeController.cs
@@ -7,13 +7,48 @@ using System.Web.Mvc;
 
 namespace N1B2_API.Controllers
 {
+    [RoutePrefix("")]
     public class AnimeController : Controller
     {
         private List<AnimeModel> listaAnimes = new List<AnimeModel>();
-        // GET: Anime
-        public ActionResult Index()
+
+        [AcceptVerbs("POST")]
+        [Route("CadastrarAnime")]
+        public string CadastrarUsuario(AnimeModel anime)
         {
-            return View();
+            listaAnimes.Add(anime);
+            return "Anime cadastrado com sucesso!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("AlterarAnime")]
+        public string AlterarAnime(AnimeModel anime)
+        {
+            listaAnimes.Where(a => a.Codigo == anime.Codigo).Select(n =>
+            {
+                n.Codigo = anime.Codigo;
+                n.NomeAnime = anime.NomeAnime;
+                n.Genero = anime.Genero;
+                return n;
+            }).ToList();
+            return "Anime alterado com sucesso!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("DeleteAnime")]
+        public string DeleteAnime(int codigo)
+        {
+            var anime = listaAnimes.Where((a) => a.Codigo == codigo).FirstOrDefault();
+            listaAnimes.Remove(anime);
+            return "Anime excluido com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("GetAnime")]
+        public AnimeModel GetAnime(int codigo)
+        {
+            var anime = listaAnimes.Where((a) => a.Codigo == codigo).FirstOrDefault();
+            return anime;
         }
     }
 }
